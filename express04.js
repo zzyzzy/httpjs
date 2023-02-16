e3ef49dfa57a55caf4072347ced8e4eb2cf9cf10
JaeSeong.Song <zzyzigy@gmail.com>
2023-02-16 17:42:30 +0900

    성적처리 1 - body-parser

--- express04.js
+++ express04.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');  // 로그 출력기
 const { engine } = require('express-handlebars');
+const bodyParser = require('body-parser');   // 폼 처리기
 
 // 라우팅 외부 작성
 const indexRouter = require('./routes/index');
@@ -30,6 +31,13 @@ app.use(express.static(path.join(__dirname, 'static')));
 
 // 로그 설정
 app.use(logger('dev'));
+
+// 미들웨어 등록 및 설정
+app.use(express.json());
+// 전송된 폼 데이터에 대한 urlencoding 설정
+app.use(express.urlencoded({extended: false}));
+app.use(bodyParser.json());   // 전송된 폼 데이터는 json형식
+// app.use(bodyParser.text());   // enctype이 text/plain일때 필요 (비추)
 
 // index에 대한 route handler 지정
 app.use('/', indexRouter);
--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,28 @@ router.get('/', (req, res) => {
     res.render('index', {title: 'index'});
 });
 
+router.get('/sungjuk', (req, res) => {
+    res.render('sungjuk', {title: '성적처리'});
+});
+
+router.post('/sungjuk', (req, res, next) => {
+    // 폼으로 전송된 데이터들은 req.body, req.body.폼이름 등으로 확인 가능
+    //console.log(req.body);
+    //console.log(req.body.name, req.body.kor, req.body.eng, req.body.mat);
+
+    let { name, kor, eng, mat } = req.body;
+    console.log(name, kor, eng, mat);
+
+    // 성적처리
+    let [tot, avg , grd] = [kor+eng+mat, (kor+eng+mat)/3, '가'];
+    console.log(tot, avg , grd);
+
+    // 데이터베이스 처리 - sungjuk 테이블에 insert
+
+
+    res.redirect(304, '/');
+});
+
 // 단순한 그림파일을 화면에 표시하기 위해
 // 일일이 라우팅 설정하는 것은 번거로움
 // router.get('/smile.png', (req, res) => {
